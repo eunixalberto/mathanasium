40483d0c72c3ff7d27f344e5d33124351daf9339
Carl Alberto <cralberto11@gmail.com>
2018-01-10 00:30:36 +0000

    helfway of the main page

--- web/wp-content/themes/mathanasium/header.php
+++ web/wp-content/themes/mathanasium/header.php
@@ -45,10 +45,11 @@
 						<?php
 					}
 				?>
+				<?php /*
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'bootstrapfast' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation --> <?php
+				</nav><!-- #site-navigation --> */ ?><?php
 				if ( bootstrapfast_main_sidebar_placement() ) {
 						get_sidebar();
 				} ?>
